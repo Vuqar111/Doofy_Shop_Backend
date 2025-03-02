a7dcdcc5aa06dcc99bbe8f9b696f3b9ae0485d5a
Vuqar111 <vuqar.hesenov_2003@mail.ru>
2025-03-02 18:13:12 +0400

    fixing bug

--- src/controllers/order/orderListController.ts
+++ src/controllers/order/orderListController.ts
@@ -1,7 +1,6 @@
-import {  Response } from "express";
+import { Response } from "express";
 import Order from "../../models/order";
 import Customer from "../../models/customer";
-import { IOrder } from "../../interfaces/order";
 import { CustomerRequest } from "../../middleware/customerMiddleware";
 
 // Get all customer orders
@@ -10,25 +9,24 @@ export const getAllCustomerOrders = async (
   res: Response
 ): Promise<void> => {
   try {
-    // Extract search query parameters from request query
     const customerId = req.customer?._id;
+
+    // Find the customer by ID and get their email
     const customer = await Customer.findById(customerId);
-    
-    // if(!customer) {
-    //     return res.status(404).json({ message: "Customer not found" });
-    // }
-    
-    let products: IOrder[];
-    let query: any = {};
+    if (!customer) {
+      res.status(404).json({ error: "Customer not found" });
+      return;
+    }
 
-    
+    const customerEmail = customer.email;
 
-    // Fetch products based on the constructed query
-    products = await Order.find({}).sort({ createdAt: -1 })
+    // Find orders associated with the customer's email
+    const orders = await Order.find({ customerId: customerEmail }).sort({ createdAt: -1 });
 
-    res.status(200).json(products);
+    res.status(200).json(orders);
   } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
 };
+
 
